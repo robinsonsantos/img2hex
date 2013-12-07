81378de6b47b7944a7b75b3a1e538132958bded5
Robinson D. S. Santos <stanleymagrao@gmail.com>
2013-12-07 18:22:51 -0200

    Addes TODO

---
+++ img2hex.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+# *-* coding: utf-8 *-*
+
+
+#TODO remove old code
+#TODO use command line parameter
+#TODO use graphic interface option
+#TODO use not only bmp files
+#TODO choose size output
+
+import Image
+import ImageColor
+
+#bmp_img = '/home/robinson/python_glcd/tux04.bmp'
+#vetor   = '/home/robinson/python_glcd/tux04.txt'
+
+bmp_img  = 'tux04.bmp'
+vetor_c =  'tux05.c'
+
+f = open(vetor_c,'w')
+i = Image.open(bmp_img)
+
+x = 0
+y = 0
+z = 0
+octet = 0
+
+f.write('const unsigned char ')
+f.write('tux04')
+#f.write('[ ')
+#f.write(str(x+y))
+#f.write(' ] = {\n\t')
+
+f.write('[] = {\n\t')
+
+
+while y < 128:
+    x = 0
+    while x < 240:
+        for j in range(8):
+            z = i.getpixel((x,y))
+
+            if (j == 0):
+                octet = (z & 1)
+            else:
+                octet = octet << 1
+                octet = octet | (z & 1)
+
+            x = x + 1
+
+        octet = hex(octet)   
+        f.write(str(octet))
+        f.write(', ')
+
+    y = y + 1
+    f.write('\n\t')
+
+f.write('};')
+f.write('\n')
+f.close()
+
+
+# muito importante essa funcao
+# import Image
+# im = Image.open("tux04.bmp")
+# list_of_pixels = list(im.getdata())
+# print list_of_pixels[0]
