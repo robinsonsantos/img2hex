454d6787c9f23eae7a3427e5b6822bc008a1ec24
Robinson Santos <engenharia_robinson@inobram.com.br>
2013-12-10 16:59:22 -0200

    Changed to object oriented

--- img2hex.py
+++ img2hex.py
@@ -1,58 +1,87 @@
-#! /usr/bin/env python
-# *-* coding: utf-8 *-*
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-
-#TODO remove old code
 #TODO use command line parameter
-#TODO use graphic interface option
+#TODO verify dependencies, otherwise suggest install
 #TODO use not only bmp files
-#TODO choose size output
-#TODO use OOP
+#TODO use output file define by user
+#TODO convert image to grayscale
 
+import sys
+import re
 import Image
-import ImageColor
 
-bmp_img  = 'tux04.bmp'
-vetor_c =  'tux05.c'
+FILE_NAME  = 1
+MAX_ROW    = 128
+MAX_COLUMN = 240
 
-f = open(vetor_c,'w')
-i = Image.open(bmp_img)
+class Img2hex(object):
+    ''' None '''
+    def __init__(self):
+        self.__img_file = None
+        self.__hex_file = None
 
-x = 0
-y = 0
-z = 0
-octet = 0
+    def open_img(self, img_file):
+        try:
+            self.__img_file = Image.open(img_file)
+            print 'Image file opened sucessfully'
+        except:
+            print 'Could not open the image file'
+            sys.exit()
+ 
+    def open_hex(self, hex_file):
+        try:
+            self.__hex_file = open(hex_file, 'w')
+            print 'Hex file opened sucessfully'
+        except:
+            print 'Could not open the hex file'
+            sys.exit()
 
-f.write('const unsigned char ')
-f.write('tux04')
-#f.write('[ ')
-#f.write(str(x+y))
-#f.write(' ] = {\n\t')
+    def convert_to_grayscale(self):
+        pass
+          
+    def generate(self):
+        column = 0
+        row = 0
+        rgb_value = 0
+        octet = 0
+        self.__hex_file.write('const unsigned char ')
+        self.__hex_file.write('tux04')
+        self.__hex_file.write('[] = {\n\t')
 
-f.write('[] = {\n\t')
+        while row < MAX_ROW:
+            column = 0
+            while column < MAX_COLUMN:
+                for i in xrange(8):
+                    rgb_value = self.__img_file.getpixel((column, row))
+
+                    if (i == 0):
+                        octet = (rgb_value & 1)
+                    else:
+                        octet = octet << 1
+                        octet = octet | (rgb_value & 1)
+
+                    column = column + 1
+
+                octet = hex(octet)
+                self.__hex_file.write(str(octet))
+                self.__hex_file.write(', ')
+
+            row = row + 1
+            self.__hex_file.write('\n\t')
+        self.__hex_file.write('};')
+        self.__hex_file.write('\n')
+        self.__hex_file.close()
 
 
-while y < 128:
-    x = 0
-    while x < 240:
-        for j in range(8):
-            z = i.getpixel((x,y))
+def main():
+    if len(sys.argv) == 2:
+        print 'Begin'
+        img2hex = Img2hex()
+        img2hex.open_img(sys.argv[FILE_NAME])
+        img2hex.open_hex('teste.c')
+        img2hex.generate()
+        print 'End'
 
-            if (j == 0):
-                octet = (z & 1)
-            else:
-                octet = octet << 1
-                octet = octet | (z & 1)
-
-            x = x + 1
-
-        octet = hex(octet)   
-        f.write(str(octet))
-        f.write(', ')
-
-    y = y + 1
-    f.write('\n\t')
-
-f.write('};')
-f.write('\n')
-f.close()
+if __name__ == "__main__":
+    main()
